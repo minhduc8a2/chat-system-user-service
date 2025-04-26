85537a24b1719e5a6ebbf5835749165502dc85e2
Duke Lee <minhduc8a2.1@gmail.com>
2025-04-26 15:37:28 +0700

    init admin data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,5 @@
 INSERT INTO users (email, auth_id, created_at, updated_at) 
 VALUES ('minhduc8a2.1@gmail.com', 1, NOW(), NOW());
+
+INSERT INTO users (email, auth_id, created_at, updated_at) 
+VALUES ('minhduc8a2.admin@gmail.com', 2, NOW(), NOW());
